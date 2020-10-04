fe1b6b124537994034180f019fe15c45f43c9416
Cemre Onur Baş <cemreonurb@gmail.com>
2020-10-04 15:40:14 +0300

    Initial Commit

---
+++ dataviz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dataviz
+//
+//  Created by Cemre Onur Baş on 4.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dataviz/datavizApp.swift
@@ -0,0 +1,17 @@
+//
+//  datavizApp.swift
+//  dataviz
+//
+//  Created by Cemre Onur Baş on 4.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct datavizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
